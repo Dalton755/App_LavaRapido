fcb89793d2240bdcfc504def1bed0ff6e8ce6f8f
Dalton755 <150269082+Dalton755@users.noreply.github.com>
2026-05-28 19:32:22 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,23 @@
 
 const API_URL = "https://script.google.com/macros/s/AKfycbxfyYCt0mtFf7SgvFx7Hz2ueqPnQ4374xvl8k2j8_tHsIezCaiIuuRTZPcxcKnX4shm/exec";
 
+const CONFIG = {
+
+  ABA:'Solicitacoes',
+
+  PASTA_FOTOS:
+    '12eXINxhfFHMpI2iMrcZLFEaMAkg_8StQ',
+
+  STATUS:{
+
+    SOLICITADO:'SOLICITADO',
+
+    AGUARDANDO:
+      'AGUARDANDO_LAVAGEM',
+
+    CONCLUIDO:'CONCLUIDO'
+
+  }
+
+};
+
