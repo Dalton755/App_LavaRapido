64c1e97dd005ef543534a62f5a8b79bafddf5a00
Dalton755 <150269082+Dalton755@users.noreply.github.com>
2026-05-28 17:29:54 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-const API_URL = "https://script.google.com/macros/s/AKfycbz7G9yqmMljlFinp7Bw4FJyxHeU1c3Qz26gGm00veX8e_C-wzUaUaBcLO6UhLbtS00t/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzRP89TKHj7XdB9vomeBrodaPWEz10xHJyMCD4kIzn65MBwVNbWG0H7GSfngnHFqfee/exec";
 
